120347648913154e70f9f3ccabadfa94f4d8e1f3
Nicholas Bailey <nicholas.j.bailey91@gmail.com>
2017-11-29 13:22:27 -0500

    Added LuaHelper

---
+++ LuaClasses/LuaHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NLua;
+
+namespace LuaClasses
+{
+	public class LuaHelper
+	{
+		public Lua State;
+
+		public LuaHelper ()
+		{
+			State = new Lua();
+			State.LoadCLRPackage ();
+		}
+
+		public void AddGlobal(string name, object o)
+		{
+			State [name] = o;
+		}
+
+		public void RegisterFunction(string name, object b, System.Reflection.MethodInfo method)
+		{
+			State.RegisterFunction (name, b, method);
+		}
+
+		public object[] Call(string name, params object[] args) 
+		{
+			LuaFunction func = State [name] as LuaFunction;
+			return func.Call (args);
+		}
+
+		public void RunFile(string path)
+		{
+			State.DoFile (path);
+		}
+	}
+}
+
